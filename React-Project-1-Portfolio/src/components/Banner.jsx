3a09ec855212735a40caecbffb4288f5106e3b4a
Sabiha Afrin <satonnee@gmail.com>
2024-01-22 16:20:13 +0600

    Update

--- React-Project-1-Portfolio/src/components/Banner.jsx
+++ React-Project-1-Portfolio/src/components/Banner.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
+import Button from "./buttons/Button";
 
 const Banner = () => {
-  return (
-      <>
-          <div className='banner'>
-              <div className="overlay">
-                  
-              </div>
-          </div>
-      </>
-  )
-}
+    return (
+        <>
+            <div className="banner">
+                <div className="overlay">
+                    <Button text='View My Works'/>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default Banner
+export default Banner;
