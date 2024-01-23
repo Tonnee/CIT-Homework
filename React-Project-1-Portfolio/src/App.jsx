0828c99579d60ec31eec7bb4ca44e92fffde25da
Sabiha Afrin <satonnee@gmail.com>
2024-01-23 09:38:19 +0600

    Update

--- React-Project-1-Portfolio/src/App.jsx
+++ React-Project-1-Portfolio/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Button from "./components/buttons/Button";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import "./App.css";
--- React-Project-1-Portfolio/src/components/Banner.jsx
+++ React-Project-1-Portfolio/src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./buttons/button.css";
 import Button from "./buttons/Button";
 
 const Banner = () => {
