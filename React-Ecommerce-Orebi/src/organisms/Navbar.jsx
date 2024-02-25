d3ef6f83eabf99f1d03e7ef1a9dc76e3cd47eda7
Sabiha Afrin <satonnee@gmail.com>
2024-02-25 12:18:44 +0600

    Update

--- React-Ecommerce-Orebi/src/organisms/Navbar.jsx
+++ React-Ecommerce-Orebi/src/organisms/Navbar.jsx
@@ -1,59 +1,76 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "../atoms/Image";
 import Logo from "../assets/orebi-logo.png";
 import Flex from "../layout/Flex";
 import List from "../layout/List";
 import ItemList from "../layout/ItemList";
+import Container from "../layout/Container";
 import { IoIosMenu } from "react-icons/io";
 
 const Navbar = () => {
-    let [show, setShow] = useState(false);
+    let [show, setShow] = useState(true);
+
+    useEffect(() => {
+        function changeWidth() {
+            if (window.innerWidth < 769) {
+                setShow(false);
+            } else {
+                setShow(true);
+            }
+        }
+
+        changeWidth();
+
+        window.addEventListener("resize", changeWidth);
+    }, []);
 
     return (
         <>
             <nav>
-                <div className="bg-white py-8">
-                    <div className="max-w-container mx-auto px-4 xl:px-0">
-                        <Flex className="justify-end relative">
+                <div className="bg-white md:py-8 py-4">
+                    <Container>
+                        <Flex className="justify-end items-center relative">
                             <div className="absolute left-0">
                                 <Image imgSrc={Logo} imgAlt="Orebi Logo" />
                             </div>
-                            {show &&
-                            
-                            <List className="flex gap-10">
-                                <ItemList
-                                    style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200"
-                                    text="Home"
-                                    link="#"
-                                />
-                                <ItemList
-                                    style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200"
-                                    text="Shop"
-                                    link="#"
-                                />
-                                <ItemList
-                                    style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200"
-                                    text="About"
-                                    link="#"
-                                />
-                                <ItemList
-                                    style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200"
-                                    text="Contacts"
-                                    link="#"
-                                />
-                                <ItemList
-                                    style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200"
-                                    text="Journal"
-                                    link="#"
-                                />
-                            </List>
-                            }
+
+                            {show && (
+                                <List className="md:flex md:gap-10 md:static absolute left-2 top-10">
+                                    <ItemList
+                                        style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200 my-2.5 md:my-0"
+                                        text="Home"
+                                        link="#"
+                                    />
+                                    <ItemList
+                                        style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200 my-2.5 md:my-0"
+                                        text="Shop"
+                                        link="#"
+                                    />
+                                    <ItemList
+                                        style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200 my-2.5 md:my-0"
+                                        text="About"
+                                        link="#"
+                                    />
+                                    <ItemList
+                                        style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200 my-2.5 md:my-0"
+                                        text="Contacts"
+                                        link="#"
+                                    />
+                                    <ItemList
+                                        style="font-dm text-sm font-normal text-gray76 hover:text-gray26 hover:font-bold hover:transition-all hover:duration-200 my-2.5 md:my-0"
+                                        text="Journal"
+                                        link="#"
+                                    />
+                                </List>
+                            )}
 
                             {/*Mobile Menu*/}
-                            <IoIosMenu className="lg:hidden block text-3xl text-black" />
+                            <IoIosMenu
+                                className="md:hidden block text-3xl text-black"
+                                onClick={() => setShow(!show)}
+                            />
                         </Flex>
-                    </div>
+                    </Container>
                 </div>
             </nav>
         </>
---
+++ React-Ecommerce-Orebi/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../organisms/Navbar";
+import Banner from "../organisms/Banner";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <Banner />
+        </>
+    );
+};
+
+export default Home;
