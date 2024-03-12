c8884bb1dd2f21c31ab2f18ac7caeb7413761985
Sabiha Afrin <satonnee@gmail.com>
2024-03-12 16:07:52 +0600

    Update

--- React-ToDo/src/App.jsx
+++ React-ToDo/src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getDatabase, ref, set, push, onValue } from "firebase/database";
 
 function App() {
+    const db = getDatabase();
     let [postData, setPostData] = useState("");
 
     let handleChange = (e) => {
         setPostData(e.target.value);
     };
-  
-  let handleSave = () => {
-      console.log(postData);
-    }
+
+    let handleSave = () => {
+        set(push(ref(db, "ToDo")), {
+            task: postData,
+        });
+    };
+
+    useEffect(() => {
+        const todoRef = ref(db, "ToDo");
+        onValue(todoRef, (snapshot) => {
+            console.log(snapshot.val());
+        });
+    }, []);
 
     return (
         <>
-            <input onChange={handleChange} />
-            <button onClick={handleSave}>Save</button>
+            <input
+                onChange={handleChange}
+                className="border-2 rounded-md borderd py-1.5 pl-7 pr-20 text-gray-900"
+            />
+            <button onClick={handleSave} className="bg-blue-600 p-2 rounded-sm">
+                Save
+            </button>
         </>
     );
 }
