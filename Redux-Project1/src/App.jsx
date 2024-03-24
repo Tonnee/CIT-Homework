d9d6688eb4aeda623a857195876f3adf4a04c924
Sabiha Afrin <satonnee@gmail.com>
2024-03-25 05:09:45 +0600

    Update

---
+++ Redux-Project1/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Minus from "./components/Minus";
+import Plus from "./components/Plus";
+import Result from "./components/Result";
+
+function App() {
+    return (
+        <>
+            <Plus />
+            <Result />
+            <Minus />
+        </>
+    );
+}
+
+export default App;
---
+++ Redux-Project1/src/components/Minus.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Minus = () => {
+  return (
+    <div>
+      Minus
+    </div>
+  )
+}
+
+export default Minus
