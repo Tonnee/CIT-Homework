cab4af930d7d9c47bf4b80b1cefb77f32f1d1618
Sabiha Afrin <satonnee@gmail.com>
2024-05-07 14:35:36 +0600

    Update

--- ChitChat/src/components/atoms/Heading.jsx
+++ ChitChat/src/components/atoms/Heading.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Heading = (props) => {
-  return (
-    props.as?<props.as>This is a heading</props.as>:<h6>This is also a heading</h6>
-  )
-}
+    return props.as ? (
+        <props.as className={props.className}>{props.text}</props.as>
+    ) : (
+        <h6 className={props.className}>{props.text}</h6>
+    );
+};
 
-export default Heading
+export default Heading;
--- ChitChat/src/components/pages/Registration.jsx
+++ ChitChat/src/components/pages/Registration.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 import Image from "../atoms/Image";
 import registrationImage from "../../assets/registration.png";
 import Heading from "../atoms/Heading";
+import Paragraph from "../atoms/Paragraph";
 
 const Registration = () => {
     return (
@@ -16,13 +18,38 @@ const Registration = () => {
                     item
                     xs={6}
                 >
-                    <Heading as="h1" />
-                    <TextField
-                        required
-                        id="outlined-required"
-                        label="Email Address"
-                        defaultValue="Ladushing691@gmail.com"
-                    />
+                    <div className="reg-div">
+                        <Heading
+                            as="h1"
+                            text="Get started with easily register"
+                        />
+                        <Paragraph text="Free register and you can enjoy it" />
+
+                        <div className="input-div">
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Email Address"
+                                type="email"
+                            />
+
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Full name"
+                                type="text"
+                            />
+
+                            <TextField
+                                required
+                                id="outlined-required"
+                                label="Password"
+                                type="password"
+                            />
+
+                            <Button variant="contained">Sign Up</Button>
+                        </div>
+                    </div>
                 </Grid>
                 <Grid
                     item
