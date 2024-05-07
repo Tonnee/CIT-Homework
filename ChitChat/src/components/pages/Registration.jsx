41a15a625019f5c06b06ae7308a345656c10d01f
Sabiha Afrin <satonnee@gmail.com>
2024-05-07 19:39:30 +0600

    update

--- ChitChat/src/components/pages/Registration.jsx
+++ ChitChat/src/components/pages/Registration.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -6,8 +6,30 @@ import Image from "../atoms/Image";
 import registrationImage from "../../assets/registration.png";
 import Heading from "../atoms/Heading";
 import Paragraph from "../atoms/Paragraph";
+import Alert from "@mui/material/Alert";
 
 const Registration = () => {
+    let [regData, setRegData] = useState({
+        email: "",
+        name: "",
+        password: "",
+    });
+
+    let [regError, setRegError] = useState({
+        email: "",
+        name: "",
+        password: "",
+    });
+
+    let handleChange = (e) => {
+        setRegData({ ...regData, [e.target.name]: e.target.value });
+    };
+
+    let handleSubmit = () => {
+        if (!regData.email) {
+            setRegError({ ...regError, email: "Email Required" });
+        }
+    };
     return (
         <>
             <Grid
@@ -31,13 +53,21 @@ const Registration = () => {
                                 id="outlined-required"
                                 label="Email Address"
                                 type="email"
+                                name="email"
+                                onChange={handleChange}
                             />
+
+                            {regError.email && (
+                                <Alert severity="error">{regError.email}</Alert>
+                            )}
 
                             <TextField
                                 required
                                 id="outlined-required"
                                 label="Full name"
                                 type="text"
+                                name="name"
+                                onChange={handleChange}
                             />
 
                             <TextField
@@ -45,9 +75,16 @@ const Registration = () => {
                                 id="outlined-required"
                                 label="Password"
                                 type="password"
+                                name="password"
+                                onChange={handleChange}
                             />
 
-                            <Button variant="contained">Sign Up</Button>
+                            <Button
+                                variant="contained"
+                                onClick={handleSubmit}
+                            >
+                                Sign Up
+                            </Button>
                         </div>
                     </div>
                 </Grid>
